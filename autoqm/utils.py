ecd927dfc53e499435331cebdfb12a03083fee07
KEHANG <kehanghan@gmail.com>
2017-05-16 17:29:25 -0400

    remove support for auth info fetched from environment vars

--- autoqm/utils.py
+++ autoqm/utils.py
@@ -14,16 +14,6 @@ def read_config(cfg_path='default'):
 	return config_parser._sections
 
 def get_TCD_authentication_info(cfg_path='default'):
-
-	try:
-		host = os.environ['TCD_HOST']
-		port = int(os.environ['TCD_PORT'])
-		username = os.environ['TCD_USER']
-		password = os.environ['TCD_PW']
-
-		return host, port, username, password
-	except KeyError:
-		print('Thermo Central Database Authentication Environment Variables Not Completely Set!')
 	
 	try:
 		config = read_config(cfg_path)
