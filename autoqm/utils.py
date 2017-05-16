9c2866af2b117f283e101e456e29f06a7396b4e0
KEHANG <kehanghan@gmail.com>
2017-05-16 17:28:57 -0400

    add default option for read_config
    so that we don't need check for default in
    get_TCD_authentication_info

--- autoqm/utils.py
+++ autoqm/utils.py
@@ -1,11 +1,14 @@
 import os
 import ConfigParser
 
-def read_config(cfg_path):
+def read_config(cfg_path='default'):
 	'''This function reads a configuration file and returns an equivalent dictionary'''
 
 	config_parser = ConfigParser.SafeConfigParser()
 	config_parser.optionxform = str
+
+	if cfg_path == 'default':
+		cfg_path = os.path.join(os.path.dirname(__file__), 'config.cfg')
 	with open(cfg_path, 'r') as fid:
 		config_parser.readfp(fid)
 	return config_parser._sections
@@ -23,9 +26,6 @@ def get_TCD_authentication_info(cfg_path='default'):
 		print('Thermo Central Database Authentication Environment Variables Not Completely Set!')
 	
 	try:
-		if cfg_path == 'default':
-			cfg_path = os.path.join(os.path.dirname(__file__), 'config.cfg')
-
 		config = read_config(cfg_path)
 
 		host = config['ThermoCentralDatabase']['TCD_HOST']
