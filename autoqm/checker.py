f407c4b5c3973742c7ff8c83cb8b9aadbf2fd696
KEHANG <kehanghan@gmail.com>
2017-05-21 10:19:50 -0400

    add checker working rationale

---
+++ autoqm/checker.py
@@ -0,0 +1,21 @@
+
+# connect to registration table
+
+# search entries with status "job_launched" or "job_running"
+# check the job slurm-status, e.g., scontrol show jobid 5037088
+# 1. Invalid job id specified: means the job is off queue
+#    1.1 check job folder if input.log file is there
+#			Yes: job already finished
+#			No:  job is aborted before even calculation start: job_aborted
+#    1.2 if finished, check "Normal termination" key words should appear twice
+#			Yes: job is converged
+#			No:  job fails convergence (possibly need more wall-time): job_failed_convergence
+#    1.3 if converged, check isomorphism between input molecule and output molecule
+#			Yes: job is success: job_success
+#			No:  job fails isomorphism (possibly need tweak on initial structure): job_failed_isomorphism
+# 2. output string starts with e.g, "JobId=5037088"
+#    2.1 check JobState
+#		RUNNING: running: job_running
+#		else:   still job_launched 
+
+# update status accordingly
