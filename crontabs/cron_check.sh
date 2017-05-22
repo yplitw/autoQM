84ff7f2e03fcd3f4f0724d338f9533efe779bd8c
KEHANG <kehanghan@gmail.com>
2017-05-22 11:18:46 -0400

    add crontab job for checking status

---
+++ crontabs/cron_check.sh
@@ -0,0 +1,13 @@
+
+export BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." && pwd )"
+export PYTHONPATH=$PYTHONPATH:$BASE_DIR/code/RMG-Py
+export PYTHONPATH=$PYTHONPATH:$BASE_DIR/code/autoQM
+export PATH=$BASE_DIR/anaconda/bin:$PATH
+
+source activate autoqm_env
+
+echo $(date +%Y-%m-%d:%H:%M:%S) 
+echo "autoQM directory: "$BASE_DIR/code/autoQM
+python $BASE_DIR/code/autoQM/autoqm/checker.py
+
+source deactivate
