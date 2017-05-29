8a8fc04358d4bccf74a4d89f5d06135ed7246875
KEHANG <kehanghan@gmail.com>
2017-05-29 15:06:46 -0400

    adds two utility functions
    one for testing database authentication
    the other for getting level of theory from
    quantum inputs

--- autoqm/utils.py
+++ autoqm/utils.py
@@ -28,3 +28,32 @@ def get_TCD_authentication_info(cfg_path='default'):
 		print('Thermo Central Database Configuration File  Not Completely Set!')
  
 	return 'None', 0, 'None', 'None'
+
+def get_level_of_theory(inp_path):
+	"""
+	This helper method returns level of theory given
+	a quantum input file. 
+
+	Currently it supports Gaussian inputs only.
+	"""
+	with open(inp_path, 'r') as f_in:
+		for line in f_in.readlines():
+			if '# opt freq ' in line:
+				level_of_theory = line.split(' ')[3].strip().lower()
+				return level_of_theory
+		else:
+			raise Exception('Can not find level of theory in {0}.'.format(inp_path))
+
+def getTestingTCDAuthenticationInfo():
+
+    try:
+        host = os.environ['TCD_HOST']
+        port = int(os.environ['TCD_PORT'])
+        username = os.environ['TCD_USER']
+        password = os.environ['TCD_PW']
+    except KeyError:
+        print('Thermo Central Database Authentication Environment Variables Not Completely Set!')
+        return 'None', 0, 'None', 'None'
+
+    return host, port, username, password
+
