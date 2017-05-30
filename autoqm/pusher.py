eafef38e77b1c56bb79d56876ca723cfeaab5bde
KEHANG <kehanghan@gmail.com>
2017-05-30 00:11:00 -0400

    add push_jobs method and its unittest

--- autoqm/pusher.py
+++ autoqm/pusher.py
@@ -18,6 +18,8 @@
 # level of theory,
 # timestamp
 import os
+import time
+import shutil
 
 import autoqm.utils
 
@@ -56,4 +58,61 @@ def select_push_target(registration_table,
 				# in results table
 				selected_targets.append(target)
 	return selected_targets
+
+def push_jobs(registration_table, results_table, success_data_path):
+
+	# select push targets
+	targets = select_push_target(registration_table,
+								results_table,
+								success_data_path)
+
+	for target in targets:
+		aug_inchi = str(target['aug_inchi'])
+		spec_name = aug_inchi.replace('/', '_slash_')
+		spec_path = os.path.join(success_data_path, spec_name)
+
+		inp_path = os.path.join(spec_path, 'input.inp')
+		model_chemistry = autoqm.utils.get_level_of_theory(inp_path)
+
+		smiles = str(target['SMILES_input'])
+
+		# run cantherm
+		thermo = autoqm.utils.run_cantherm(spec_path, model_chemistry, smiles)
+		
+		# push to results table
+		Hf298 = thermo.H298.value_si/1000/4.184  # kcal/mol
+		S298 = thermo.S298.value_si/4.184		 # cal/mol/K
+		Cp300 = thermo.Cpdata.value_si[0]/4.184  # cal/mol/K
+		Cp400 = thermo.Cpdata.value_si[1]/4.184  # cal/mol/K
+		Cp500 = thermo.Cpdata.value_si[2]/4.184  # cal/mol/K
+		Cp600 = thermo.Cpdata.value_si[3]/4.184  # cal/mol/K
+		Cp800 = thermo.Cpdata.value_si[4]/4.184  # cal/mol/K
+		Cp1000 = thermo.Cpdata.value_si[5]/4.184 # cal/mol/K
+		Cp1500 = thermo.Cpdata.value_si[6]/4.184 # cal/mol/K
+
+		# check results table again there's no
+		# same entry
+		res_query = {"aug_inchi":aug_inchi, 
+					"level_of_theory":model_chemistry}
+		res_entries = list(results_table.find(res_query))
+		
+		if len(res_entries) > 0:
+			continue
+
+		# do the insertion
+		insert_entry = {"aug_inchi" : aug_inchi, 
+						"Hf298(kcal/mol)" : Hf298, 
+						"S298(cal/mol/K)" : S298, 
+						"Cp300(cal/mol/K)" : Cp300, 
+						"Cp400(cal/mol/K)" : Cp400, 
+						"Cp500(cal/mol/K)" : Cp500, 
+						"Cp600(cal/mol/K)" : Cp600, 
+						"Cp800(cal/mol/K)" : Cp800, 
+						"Cp1000(cal/mol/K)" : Cp1000, 
+						"Cp1500(cal/mol/K)" : Cp1500, 
+						"timestamp" : time.time(), 
+						"SMILES_input" : smiles, 
+						"level_of_theory" : model_chemistry}
+
+		results_table.insert_one(insert_entry)
 
--- test/pusher_test.py
+++ test/pusher_test.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 
 import autoqm.pusher
@@ -37,3 +38,31 @@ class TestPusher(unittest.TestCase):
     	self.assertEqual(str(selected_targets[0]['aug_inchi']), 
     					'InChI=1S/C10H12/c1-3-7-8(4-1)10-6-2-5-9(7)10/h1-3,6-10H,4-5H2')
 
+    def test_push_jobs(self):
+
+        pusher_reg_table = getattr(self.tcd, 'pusher_reg_table')
+        pusher_res_table = getattr(self.tcd, 'pusher_res_table')
+        success_data_path = os.path.join(os.path.dirname(__file__), 
+                                        'data', 
+                                        'pusher_data',
+                                        'success')
+
+        # check/clean-up pusher_res_table before pushing
+        self.assertEqual(1, pusher_res_table.count())
+
+        # push
+        autoqm.pusher.push_jobs(pusher_reg_table, pusher_res_table, success_data_path)
+
+        # check pusher_res_table after pushing
+        self.assertEqual(2, pusher_res_table.count())
+
+        # clean up pusher_res_table
+        # and test data folder
+        added_aug_inchi = 'InChI=1S/C10H12/c1-3-7-8(4-1)10-6-2-5-9(7)10/h1-3,6-10H,4-5H2'
+        pusher_res_table.delete_many({"aug_inchi": added_aug_inchi})
+
+        spec_name = added_aug_inchi.replace('/', '_slash_')
+        shutil.rmtree(os.path.join(success_data_path, spec_name, 'cantherm'))
+
+        # check pusher_res_table again
+        self.assertEqual(1, pusher_res_table.count())
