fddbe801467b15f0094fe54562edd31f33bd66a4
KEHANG <kehanghan@gmail.com>
2017-12-14 08:32:39 -0500

    some people might use miniconda, add that option as well

--- crontabs/cron_launch.sh
+++ crontabs/cron_launch.sh
@@ -3,6 +3,9 @@ export BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." && pwd )"
 export PYTHONPATH=$PYTHONPATH:$BASE_DIR/code/RMG-Py
 export PYTHONPATH=$PYTHONPATH:$BASE_DIR/code/autoQM
 export PATH=$BASE_DIR/anaconda/bin:$PATH
+# some might use miniconda
+# uncomment the line below if so
+# export PATH=$BASE_DIR/miniconda/bin:$PATH
 
 source activate autoqm_env
 
